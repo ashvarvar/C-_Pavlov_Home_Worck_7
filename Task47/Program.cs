2523e9bf93d21798e41474cecb9e55a2bbe250f4
ashvarvar <ashvarvar@gmail.com>
2022-05-19 00:15:18 +0300

    done

---
+++ Task47/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+double[,] GetTableDouble(int m, int n)
+{
+    double[,] table = new double[m, n];
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            table[i, j] = new Random().NextDouble() * 100;
+        }
+    }
+    return table;
+}
+
+void PrintTable(double[,] tab)
+{
+    for (int i = 0; i < tab.GetLength(0); i++)
+    {
+        for (int j = 0; j < tab.GetLength(1); j++)
+        {
+            System.Console.Write(tab[i, j] + "    ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int m = 8;
+int n = 4;
+double[,] TableDouble = GetTableDouble(m, n);
+PrintTable(TableDouble);
